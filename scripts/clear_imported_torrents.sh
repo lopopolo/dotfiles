7d46a482f56d01c72aac4aff92c0383b87366118
Ryan Lopopolo <rjl@hyperbo.la>
2011-11-08 15:43:31 -0500

    Check to see if there are any imported torrents before removing them

--- scripts/clear_imported_torrents.sh
+++ scripts/clear_imported_torrents.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
 cd /Users/lopopolo/Downloads
-rm *.torrent.imported
+
+shopt -s nullglob
+found=0
+for i in *.torrent.imported; do
+  found=1
+done
+shopt -u nullglob
+
+[ $found -eq 1 ] && rm *.torrent.imported
 
