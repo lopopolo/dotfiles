966d61f0dce70b1a2cfd3d67c9c86a5a804fb7da
Ryan Lopopolo <rjl@hyperbo.la>
2011-11-14 14:10:51 -0500

    One cronjob to rule them all

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-cd /var/www/hyperbola/wiki
-git pull
-git push
+cd /home/lopopolo/repos/wiki
+sudo -u lopopolo git pull
+sudo -u lopopolo git push
+
+chgrp -R web /home/lopopolo/repos/wiki
+chmod -R g+w /home/lopopolo/repos/wiki
 
