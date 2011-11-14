15521fe0c08a861a73b242d7fb8404b0962096a3
Ryan Lopopolo <rjl@hyperbo.la>
2011-11-14 15:36:35 -0500

    Silence output so cron stops sending me mail

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 cd /home/lopopolo/repos/wiki
-sudo -u lopopolo git pull
-sudo -u lopopolo git push
+sudo -u lopopolo git pull &> /dev/null
+sudo -u lopopolo git push &> /dev/null
 
 chgrp -R web /home/lopopolo/repos/wiki
 chmod -R g+w /home/lopopolo/repos/wiki
