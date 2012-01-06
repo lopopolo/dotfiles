cc148954ccad1d6728f2c929e6243e416e25c056
Ryan Lopopolo <rjl@hyperbo.la>
2012-01-06 17:10:05 -0500

    Remove torrents function

--- scripts/bash_functions.sh
+++ scripts/bash_functions.sh
@@ -1,17 +1,3 @@
-function torrents
-{
-echo "Enter password for lopopolo@thekremlin.dyndns.org when prompted"
-scp ~/Downloads/*.torrent lopopolo@thekremlin.dyndns.org:/Users/lopopolo/Downloads/
-if [ $? -eq 0 ]; then
-  echo "torrent files successfully copied to thekremlin.dyndns.org"
-  echo "moving local torrent files to trash"
-  mv ~/Downloads/*.torrent ~/.Trash/
-else
-  echo "scp failed"
-  return 1
-fi
-}
-
 function freq
 {
   cat ~/.*h_h*|cut -d" " -f1|ruby -e'h=Hash.new 0;h[$_]+=1 while gets;h.each{|k,v|puts"#{v} "+k}'|sort -rn|head
