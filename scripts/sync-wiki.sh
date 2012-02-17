6ae941c8250d41a44a6e80ab095f00f67e25a010
Ryan Lopopolo <rjl@hyperbo.la>
2012-02-16 20:14:48 -0500

    Run sync-wiki.sh as lopopolo instead of root

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
 cd /home/lopopolo/repos/wiki
-sudo -u lopopolo git pull &> /dev/null
-sudo -u lopopolo git push &> /dev/null
-
-chgrp -R web /home/lopopolo/repos/wiki
-chmod -R g+w /home/lopopolo/repos/wiki
+git pull &> /dev/null
+git push &> /dev/null
 
