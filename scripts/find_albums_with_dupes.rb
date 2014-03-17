27804ac56283057187328e870e1e5e902574e91f
Ryan Lopopolo <rjl@hyperbo.la>
2014-03-16 18:43:45 -0700

    improve error handling

--- scripts/find_albums_with_dupes.rb
+++ scripts/find_albums_with_dupes.rb
@@ -16,7 +16,7 @@ MUSIC_DIR = File.expand_path "~/Music/iTunes/iTunes Media/Music"
 abort "Cannot find iTunes directory. This script only runs on OS X" if not Pathname.new(MUSIC_DIR).exist?
 
 TRACK_COUNTS = Hash.new { |h, k| h[k] = Hash.new(0) }
-NO_DISC = "1"
+NO_DISC = "single disc album"
 
 num_dupe_albums = 0
 (Dir.glob(File.join(MUSIC_DIR, "*")) - %w[. ..]).each do |artist_path|
@@ -35,9 +35,19 @@ num_dupe_albums = 0
 
     TRACK_COUNTS.each do |disc, tracks|
       if tracks.values.max.nil?
-        $stderr.puts "No tracks found for #{artist}/#{album}"
+        if disc.nil? || disc == NO_DISC
+          $stderr.puts "No tracks found for #{artist}/#{album}"
+        else
+          $stderr.puts "No tracks found for #{artist}/#{album} (disc #{disc})"
+        end
+        $stderr.puts TRACK_COUNTS.inspect
       elsif tracks.values.max > 1
-        $stderr.puts "#{artist}/#{album} (disc #{disc})"
+        if disc.nil? || disc == NO_DISC
+          $stderr.puts "#{artist}/#{album}"
+        else
+          $stderr.puts "#{artist}/#{album} (disc #{disc})"
+        end
+        $stderr.puts TRACK_COUNTS.inspect
         num_dupe_albums += 1
       end
     end
