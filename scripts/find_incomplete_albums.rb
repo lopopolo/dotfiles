857811ade497b06a0bfb70a60739e880345792b7
Ryan Lopopolo <rjl@hyperbo.la>
2014-03-16 18:43:34 -0700

    Update find_incomplete_albums.rb for multidisc music
    improve error handling

--- scripts/find_incomplete_albums.rb
+++ scripts/find_incomplete_albums.rb
@@ -11,46 +11,48 @@
 # ▶ find ~/Music -iname "*feat.*mix*"
 # ▶ find ~/Music -iname "*(feat.*(*"
 
-# yes, its hard-coded for macs. Get over it.
+require "pathname"
+
+# OS X only
 MUSIC_DIR = File.expand_path "~/Music/iTunes/iTunes Media/Music"
 
-# let's not create 30k copies of this regex and another 30k hashes, mmkay?
-IGNORE_REGEXP = Regexp.compile(/^\./)
-TRACKS = Hash.new { Array.new }
+abort "Cannot find iTunes directory. This script only runs on OS X" if not Pathname.new(MUSIC_DIR).exist?
+
+TRACKS = Hash.new { |h, k| h[k] = Array.new }
+NO_DISC = "single disc album"
 
 num_incomplete_albums = 0
-Dir.new(MUSIC_DIR).each do |artist|
-  next if artist =~ IGNORE_REGEXP # ignore hidden/special files and directories
-  Dir.new(artist_dir = File.join(MUSIC_DIR, artist)).each do |album|
-    next if album =~ IGNORE_REGEXP
-    TRACKS.clear # you mean we can reuse data structures?!?!
-    Dir.new(File.join(artist_dir, album)).each do |song|
+(Dir.glob(File.join(MUSIC_DIR, "*")) - %w[. ..]).each do |artist_path|
+  artist = Pathname.new(artist_path).basename.to_path
+
+  (Dir.glob(File.join(artist_path, "*")) - %w[. ..]).each do |album_path|
+    album = Pathname.new(album_path).basename.to_path
+    TRACKS.clear
+
+    (Dir.glob(File.join(album_path, "*")) - %w[. ..]).each do |song_path|
+      song = Pathname.new(song_path).basename.to_path
       # assume tracks are numbered in the standard
       # iTunes way: optional_disk_number-track
-      TRACKS[$1] = TRACKS[$1] << $2.to_i if song =~ /^(\d\d?-)?(\d\d)/
+      TRACKS[($1 || NO_DISC).chomp("-")] << $2.to_i if song =~ /^(\d\d?-)?(\d\d)/
     end
 
-    bad = false
-    TRACKS.each_key do |key|
-      # check that the album
-      track_num_range = (TRACKS[key].min..TRACKS[key].max).to_a
-      bad = true if track_num_range.sort != TRACKS[key].sort || TRACKS[key].min > 1
-    end
-    # check to see if the bad designation is due to a multidisk album with
-    # continuous track numbering
-    if bad
-      all = TRACKS.values.flatten
-      track_num_range = (all.min..all.max).to_a
-      bad = false if track_num_range.sort == all.sort && all.min == 1
-    end
-    # if still bad, do the output
-    if bad || TRACKS.length > 1 && TRACKS.has_key?(nil)
-      puts "#{artist}/#{album}"
-      num_incomplete_albums += 1
+    TRACKS.each do |disc, track_numbers|
+      # check that each disc in the album contains a continuous range of track numbers
+      # that starts at 1
+      track_num_range = (track_numbers.min..track_numbers.max).to_a
+      if track_num_range.sort != track_numbers.sort || track_numbers.min > 1
+        if disc.nil? || disc == NO_DISC
+          $stderr.puts "#{artist}/#{album}"
+        else
+          $stderr.puts "#{artist}/#{album} (disc #{disc})"
+        end
+        $stderr.puts "=> #{TRACKS.inspect}"
+        num_incomplete_albums += 1
+      end
     end
   end
 end
 # by this point, we've looked at every file in MUSIC_DIR. Optimization say, what?
 
-puts "\n#{num_incomplete_albums} incomplete albums"
+puts "#{num_incomplete_albums} incomplete albums"
 
