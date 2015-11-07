f526771146d20e7487f5609d51491554cfd218fa
Ryan Lopopolo <rjl@hyperbo.la>
2015-11-06 21:23:24 -0800

    Fix quoting issues in readlink-f.sh

--- scripts/readlink-f.sh
+++ scripts/readlink-f.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TARGET_FILE=$1
+TARGET_FILE="$1"
 
 cd `dirname "$TARGET_FILE"`
 TARGET_FILE=`basename "$TARGET_FILE"`
@@ -16,5 +16,5 @@ done
 # Compute the canonicalized name by finding the physical path
 # for the directory we're in and appending the target file.
 PHYS_DIR=`pwd -P`
-RESULT=$PHYS_DIR/$TARGET_FILE
-echo $RESULT
+RESULT="$PHYS_DIR/$TARGET_FILE"
+echo "$RESULT"
