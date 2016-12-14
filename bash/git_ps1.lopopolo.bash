6af4642a97ff915aae2c51e07925d4ec765f6ed8
Ryan Lopopolo <rjl@hyperbo.la>
2016-12-13 23:42:45 -0600

    lint: shellcheck
    - avoid $?
    - use read-only local vars

--- bash/git_ps1.lopopolo.bash
+++ bash/git_ps1.lopopolo.bash
@@ -9,20 +9,18 @@ function git_ps1_lopopolo
   fi
 
   if [ -d "$(git rev-parse --git-dir 2> /dev/null)" ]; then
-    local git_string="$(__git_ps1 "%s")"
-    local color=""
-    git diff --ignore-submodules --no-ext-diff --quiet --exit-code
-    if [ $? -eq 0 ]; then
+    local -r git_string="$(__git_ps1 "%s")"
+    local color
+    if git diff --ignore-submodules --no-ext-diff --quiet --exit-code; then
       color="$GREEN"
     else
       color="$YELLOW"
     fi
 
     # test to see if any of the given remotes exist
-    local remote=""
+    local remote
     while read -r candidate; do
-      &>/dev/null git ls-remote --exit-code . "$candidate"
-      if [ $? -eq 0 ]; then
+      if git ls-remote --quiet --exit-code . "$candidate" > /dev/null; then
         remote="$candidate"
         break
       fi
@@ -32,13 +30,12 @@ function git_ps1_lopopolo
     local upstreamstate=""
     if [ ! -z "$remote" ]; then
       # requires git 1.7.3
-      local aheadbehind=""
-      git rev-list --quiet "${remote}"...HEAD
-      if [ $? -eq 0 ]; then
+      local aheadbehind
+      if git rev-list --quiet "${remote}"...HEAD; then
         aheadbehind="$(git rev-list --count --left-right "${remote}"...HEAD)"
       fi
 
-      local regex='([^[:space:]]+)[[:space:]]*(.*)'
+      local -r regex='([^[:space:]]+)[[:space:]]*(.*)'
       if [[ "$aheadbehind" =~ $regex ]]; then
         [[ "${BASH_REMATCH[1]}" != "0" ]] && upstreamstate="$upstreamstate -behind[${BASH_REMATCH[1]}]"
         [[ "${BASH_REMATCH[2]}" != "0" ]] && upstreamstate="$upstreamstate +ahead[${BASH_REMATCH[2]}]"
@@ -46,8 +43,8 @@ function git_ps1_lopopolo
     fi
 
     # set up repo root detection
-    local repo_root="$(basename "$(dirname "$(readlink-f.sh "$(git rev-parse --git-dir 2> /dev/null)")")")"
-    local repo_root_string=""
+    local -r repo_root="$(basename "$(dirname "$(readlink-f.sh "$(git rev-parse --git-dir 2> /dev/null)")")")"
+    local repo_root_string
     if [ ! -z "$repo_root" ]; then
       repo_root_string=" in $repo_root"
     fi
