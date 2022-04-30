72d33a8749756daf376823157d7c763673f11053
Ryan Lopopolo <rjl@hyperbo.la>
2022-04-29 22:30:07 -0700

    Remove pyenv virtualenv

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -67,7 +67,6 @@ fi
 
 if command -v pyenv >/dev/null; then
   eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
 
 # =========================================================================== #
