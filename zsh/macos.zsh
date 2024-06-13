42e4b72cf09431716b8eb9fdbf9f108bd43f4e55
Ryan Lopopolo <rjl@hyperbo.la>
2024-06-13 13:56:47 -0700

    Fix homebrew loading

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -2,9 +2,11 @@
 # Homebrew setup                                                              #
 # =========================================================================== #
 
-if type brew &>/dev/null
-then
+if [ -f /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+if type brew &>/dev/null; then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 fi
 
